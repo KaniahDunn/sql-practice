244c619616adf320ba96fc9573a6b401e9bc06d8
Kaniah Dunn <kaniahdunn@gmail.com>
2020-02-02 19:48:57 -0500

    inserting data into the table

--- index.php
+++ index.php
@@ -7,9 +7,19 @@
 SELECT * FROM celebs;
 
 
-// SQL statement that is creating a table in the database that is creating the columns id, name and age with the datatype values of integer, text, and integer respectively. 
+// SQL statement that is creating a table in the database that is creating the columns id, name and age with the datatype values of integer, text, and integer respectively.
 CREATE TABLE celebs (
 id INTEGER,
 name TEXT,
 age INTEGER
 );
+
+// inserting new data in a table
+
+INSERT INTO celebs (id, name, age) VALUES (1, 'Justin Bieber', 22 );
+
+INSERT INTO celebs (id, name, age) VALUES (2, ' Beyonce Knowles', 33);
+
+INSERT INTO celebs (id, name, age) VALUES (3, 'Jeremy Lin', 26);
+
+INSERT INTO celebs (id, name, age) VALUES (4, '  Taylor Swift', 26);
