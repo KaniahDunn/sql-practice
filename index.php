1141c94ce14c2d946ccea3152969898e5f78ae5e
Kaniah Dunn <kaniahdunn@gmail.com>
2020-02-02 19:53:44 -0500

    alerting tables and adding in more data

--- index.php
+++ index.php
@@ -15,7 +15,6 @@ age INTEGER
 );
 
 // inserting new data in a table
-
 INSERT INTO celebs (id, name, age) VALUES (1, 'Justin Bieber', 22 );
 
 INSERT INTO celebs (id, name, age) VALUES (2, ' Beyonce Knowles', 33);
@@ -23,3 +22,8 @@ INSERT INTO celebs (id, name, age) VALUES (2, ' Beyonce Knowles', 33);
 INSERT INTO celebs (id, name, age) VALUES (3, 'Jeremy Lin', 26);
 
 INSERT INTO celebs (id, name, age) VALUES (4, '  Taylor Swift', 26);
+
+//alerting a table and adding in new data content
+ALTER TABLE celebs ADD COLUMN twitter_handle TEXT;
+
+SELECT * FROM celebs;
